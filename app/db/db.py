e89e669a9822a2005786289028bc09d449d28363
Matthew Rastovac <matthewrastovac@yahoo.com>
2018-07-01 22:30:11 -0700

    Added startup updating and deleting to db functions

--- app/db/db.py
+++ app/db/db.py
@@ -4,19 +4,46 @@ from flask import Flask, jsonify, request, json, abort
 from flask_pymongo import PyMongo
 import sys
 
+from pprint import pprint
+
+
+# STARTUPS
+
 
 
 # Adds a new startup to the platform
 def add_startup(mongo, request_json):
-    for u in mongo.db.startups.find():
-        if u["name"] == request_json["name"]:
-            return False;
-
-    user_id = mongo.db.startups.insert({
+    startup = mongo.db.startups.find_one({"name": request_json["name"]})
+    if startup is not None:
+        return False
+    startup_id = mongo.db.startups.insert({
         "name": request_json["name"],
     })
+    return True
 
-    return True;
+# Modifies the data of a startup
+# NOTE: All data passed in to request_json will overwrite old data
+# NOTE: If the startup needs to be renamed, enter the new name in the new_name field of request_json
+def update_startup(mongo, request_json):
+    startup = mongo.db.startups.find_one({"name": request_json["name"]})
+    if startup is None:
+        return False
+    if request_json.get("new_name") is not None and mongo.db.startups.find_one({"name": request_json["new_name"]}) is not None:
+        return False
+    mongo.db.startups.update_one({
+        "_id": startup["_id"]},
+        {"$set": {"name": request_json["new_name"]}
+        }, True)
+    return True
+
+# Removes a startup from the platform
+def remove_startup(mongo, request_json):
+    startup = mongo.db.startups.find_one({"name": request_json["name"]})
+    if startup is None:
+        return False
+    mongo.db.startups.remove(startup["_id"])
+    return True
+
 
 
 if __name__ == "__main__":
