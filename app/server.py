e05f3f3ec35d14a88656642605ce188f8f743900
Matthew Rastovac <matthewrastovac@yahoo.com>
2018-07-02 19:17:45 -0700

    Added basic homepage
    Homepage lists name of all startups in database

--- app/server.py
+++ app/server.py
@@ -1,7 +1,10 @@
 import sys, os
+
 from flask import Flask, jsonify, request, json, abort, render_template
 from flask_pymongo import PyMongo
 from pprint import pprint
+
+from db.db import get_all_startups
 
 app = Flask(__name__)
 
@@ -11,7 +14,8 @@ app = Flask(__name__)
 # Currently only populates a table of rates
 @app.route("/")
 def main():
-    return render_template('home.html')
+    startups = get_all_startups(app.config['MONGO_INSTANCE'])
+    return render_template('home.html', startups=startups)
 
 
 # MISCELLANIOUS
@@ -20,11 +24,12 @@ def run_server(db_name='vcp', db_uri='mongodb://localhost:27017/vcp', debug=Fals
     app.config['MONGO_DBNAME'] = db_name
     app.config['MONGO_URI'] = db_uri
     mongo = PyMongo(app)
+    app.config['MONGO_INSTANCE'] = mongo
 
     # NOTE: debug must be false to allow running as a separate thread
-    app.run(debug=debug,host="0.0.0.0")
+    app.run(debug=debug, host="0.0.0.0")
 
 
 # NOTE: This will also run if run_server is created as a top-level process
 if __name__ == '__main__':
-    run_server('vcp', 'mongodb://localhost:27017/stratum', debug=True)
+    run_server('vcp', 'mongodb://localhost:27017/vcp', debug=True)
